ae6fe715154375b5bdbaa066b543b5ab893a13d1
Keegan Nair <keegan.nair@hotmail.com>
2020-02-03 18:52:27 +1100

    missed a dot

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(methodOverride('_method'));
 require("./routes/html-routes")(app);
 require("./routes/api-routes")(app);
 
-if(process,env.JAWSDB_URL){
+if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.host)
 } else{
   connection = mysql.createConnection({
